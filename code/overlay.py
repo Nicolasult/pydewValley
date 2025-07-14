fe77c1649f4f03bac870a2129081d1ab36f8bdd0
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-14 23:39:55 +0200

    display tool currently in use

--- code/overlay.py
+++ code/overlay.py
@@ -11,4 +11,13 @@ class Overlay:
         # imports
         overlay_path = "graphics/overlay/"
         self.tools_surf = {tool: pygame.image.load(f"{overlay_path}{tool}.png").convert_alpha() for tool in player.tools}
-        self.seeds_surf = {ool: pygame.image.load(f"{overlay_path}{seed}.png").convert_alpha() for seed in player.seeds}
+        self.seeds_surf = {seed: pygame.image.load(f"{overlay_path}{seed}.png").convert_alpha() for seed in player.seeds}
+
+    def display(self):
+
+        # display tools
+        tool_surf = self.tools_surf[self.player.selected_tool]
+        tool_rect = tool_surf.get_rect(midbottom = OVERLAY_POSITIONS["tool"])
+        self.display_surface.blit(tool_surf, tool_rect)
+
+        # display seeds
