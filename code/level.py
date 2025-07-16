1724c4ea6ae30f1910058aed1be97b40bafa2bba
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-16 22:49:09 +0200

    add collisions to maps edges and water

--- code/level.py
+++ code/level.py
@@ -50,6 +50,10 @@ class Level:
         for obj in tmx_data.get_layer_by_name("Decoration"):
             WildFlower((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites])
 
+        # collision tiles
+        for x, y, surf in tmx_data.get_layer_by_name("Collision").tiles():
+            Generic((x * TILE_SIZE, y * TILE_SIZE), pygame.Surface((TILE_SIZE, TILE_SIZE)), self.collision_sprites)
+
         # player
         for obj in tmx_data.get_layer_by_name("Player"):
             if obj.name == "Start":
