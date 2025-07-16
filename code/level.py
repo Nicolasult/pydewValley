32062cd09fd687056defb6b81e9d09bf7e3cd1a1
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-16 22:45:55 +0200

    change player spawn position

--- code/level.py
+++ code/level.py
@@ -50,7 +50,10 @@ class Level:
         for obj in tmx_data.get_layer_by_name("Decoration"):
             WildFlower((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites])
 
-        self.player = Player((640, 360), self.all_sprites, self.collision_sprites)
+        # player
+        for obj in tmx_data.get_layer_by_name("Player"):
+            if obj.name == "Start":
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
         Generic(pos =(0,0), 
                 surf = pygame.image.load("graphics/world/ground.png").convert_alpha(), 
                 groups = self.all_sprites, 
