9e3e585b30af429ad49f11aa1c2cd1ed540e1154
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-18 23:05:16 +0200

    fix damage method and timer

--- code/player.py
+++ code/player.py
@@ -50,8 +50,9 @@ class Player(pygame.sprite.Sprite):
         if self.selected_tool == "hoe":
             pass
         if self.selected_tool == "axe":
-            for tree in self.tree_sprites():
-                pass
+            for tree in self.tree_sprites.sprites():
+                if tree.rect.collidepoint(self.target_pos):
+                    tree.damage()
         if self.selected_tool == "water":
             pass
 
