e025d879815d1bd1df435eed70b28bda11b0d715
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-20 17:42:44 +0200

    add soild grid method and logic

--- code/soil.py
+++ code/soil.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from pytmx.util_pygame import load_pygame
 
 class SoilLayer:
     def __init__(self, all_sprites):
@@ -10,3 +11,15 @@ class SoilLayer:
 
         # graphics
         self.soil_surf = pygame.image.load("graphics/soil/o.png").convert_alpha()
+
+        self.create_soil_grid()
+    
+    def create_soil_grid(self):
+        ground = pygame.image.load("graphics/world/ground.png")
+        h_tiles, v_tiles = ground.get_width() // TILE_SIZE, ground.get_height() // TILE_SIZE
+
+        self.grid = [[[] for col in range(h_tiles)] for row in range(v_tiles)]
+        for x, y, _ in load_pygame("data/map.tmx").get_layer_by_name("Farmable").tiles():
+            self.grid[y][x].append("F")
+            
+
