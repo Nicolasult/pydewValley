d5b26feff605adf4f2a1f2c45062623d6d040ac0
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-20 17:56:58 +0200

    target farmable tiles

--- code/level.py
+++ code/level.py
@@ -20,10 +20,10 @@ class Level:
         self.tree_sprites = pygame.sprite.Group()
         self.interaction_sprites = pygame.sprite.Group()
 
+        self.soil_layer = SoilLayer(self.all_sprites)
         self.setup()
         self.overlay = Overlay(self.player)
         self.transition = Transition(self.reset, self.player)
-        self.soil_layer = SoilLayer(self.all_sprites)
 
     def setup(self):
         tmx_data = load_pygame("data/map.tmx")
@@ -73,7 +73,8 @@ class Level:
                     group = self.all_sprites, 
                     collision_sprites = self.collision_sprites,
                     tree_sprites = self.tree_sprites,
-                    interaction = self.interaction_sprites)
+                    interaction = self.interaction_sprites,
+                    soil_layer = self.soil_layer)
                 
             if obj.name == "Bed":
                 Interaction((obj.x, obj.y), (obj.width, obj.height), self.interaction_sprites, obj.name)
--- code/soil.py
+++ code/soil.py
@@ -13,7 +13,8 @@ class SoilLayer:
         self.soil_surf = pygame.image.load("graphics/soil/o.png").convert_alpha()
 
         self.create_soil_grid()
-    
+        self.create_hit_rects()
+        
     def create_soil_grid(self):
         ground = pygame.image.load("graphics/world/ground.png")
         h_tiles, v_tiles = ground.get_width() // TILE_SIZE, ground.get_height() // TILE_SIZE
@@ -21,5 +22,22 @@ class SoilLayer:
         self.grid = [[[] for col in range(h_tiles)] for row in range(v_tiles)]
         for x, y, _ in load_pygame("data/map.tmx").get_layer_by_name("Farmable").tiles():
             self.grid[y][x].append("F")
-            
+    
+    def create_hit_rects(self):
+        self.hit_rects = []
+        for index_row, row in enumerate(self.grid):
+            for index_col, cell in enumerate(row):
+                if "F" in cell:
+                    x = index_col * TILE_SIZE
+                    y = index_row * TILE_SIZE
+                    rect = pygame.Rect(x, y, TILE_SIZE, TILE_SIZE)
+                    self.hit_rects.append(rect)
 
+    def get_hit(self, point):
+        for rect in self.hit_rects:
+            if rect.collidepoint(point):
+                x = rect.x // TILE_SIZE
+                y = rect.y // TILE_SIZE
+
+                if "F" in self.grid[y][x]:
+                    print("farmable")
