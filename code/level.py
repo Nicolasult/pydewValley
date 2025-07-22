48d51eb8119a352b507c1f03b3a905bf0ef6fe34
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-22 19:49:43 +0200

    update inventory and add particle effect on harvesting

--- code/level.py
+++ code/level.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic, Water, WildFlower, Tree, Interaction
+from sprites import Generic, Water, WildFlower, Tree, Interaction, Particle
 from pytmx.util_pygame import load_pygame
 from support import *
 from transition import Transition
@@ -115,7 +115,10 @@ class Level:
         if self.soil_layer.plant_sprites:
             for plant in self.soil_layer.plant_sprites.sprites():
                 if plant.harvestable and plant.rect.colliderect(self.player.hitbox):
+                    self.player_add(plant.plant_type)
                     plant.kill()
+                    Particle(plant.rect.topleft, plant.image, self.all_sprites, LAYERS["main"])
+                    self.soil_layer.grid[row][col].remov("P")
 
     def run(self, dt):
         self.display_surface.fill("black")
@@ -132,6 +135,7 @@ class Level:
         # transition overlay
         if self.player.sleep:
             self.transition.play()
+        # print(self.player.item_inventory)
 
 class CameraGroup(pygame.sprite.Group):
     def __init__(self):
