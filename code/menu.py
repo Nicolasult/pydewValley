95a12dc07ed350504e97b366234721b26f13d1fd
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-23 22:56:31 +0200

    add input method and handle inventory options

--- code/menu.py
+++ code/menu.py
@@ -9,5 +9,22 @@ class Menu:
         self.display_surface = pygame.display.get_surface()
         self.font = pygame.font.Font("font/LycheeSoda.ttf", 30)
 
+        # options
+        self.width = 400
+        self.space = 10
+        self.padding = 8
+
+        # entries
+        self.options = list(self.player.item_inventory.keys()) + list(self.player.seed_inventory.keys())
+        self.sell_border = len(self.player.item_inventory) - 1
+
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_ESCAPE]:
+            self.toggle_menu()
+
     def update(self):
+        self.input()
         self.display_surface.blit(pygame.Surface((1000, 1000)), (0, 0))
