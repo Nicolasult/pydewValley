363d2642b278c1d726a44a50a492908434333d60
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-23 22:51:25 +0200

    setup menu display

--- code/menu.py
+++ code/menu.py
@@ -6,5 +6,8 @@ class Menu:
         # general setup
         self.player = player
         self.toggle_menu = toggle_menu
-        self.display_surface = pygame.display.get_surf()
+        self.display_surface = pygame.display.get_surface()
         self.font = pygame.font.Font("font/LycheeSoda.ttf", 30)
+
+    def update(self):
+        self.display_surface.blit(pygame.Surface((1000, 1000)), (0, 0))
