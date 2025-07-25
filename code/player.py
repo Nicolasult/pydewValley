0e3854c15d80365b7bfea29efc06f6859be38f30
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-25 18:32:55 +0200

    fix minor issues and finish the game

--- code/player.py
+++ code/player.py
@@ -156,7 +156,6 @@ class Player(pygame.sprite.Sprite):
                 self.selected_seed = self.seeds[self.seed_index]
 
             if keys[pygame.K_RETURN]:
-                self.toggle_shop()
                 collided_interaction_sprite = pygame.sprite.spritecollide(self, self.interaction, False)
                 if collided_interaction_sprite:
                     if collided_interaction_sprite[0].name == "Trader":
